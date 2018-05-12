7e9dc89658838b568efecf03ddc2aac05621e6ed
JH220 <jh220.janhendrik@gmail.com>
2018-05-12 22:10:32 +0200

    neuer workkbench und gegenstände gelöscht

--- Workbench/Assets/SCRIPTE/Touchevent.cs
+++ Workbench/Assets/SCRIPTE/Touchevent.cs
@@ -1,27 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Touchevent : MonoBehaviour {
-    int Score;
+	int zug = 0;
+	int punkte = 0;
+	public Randomobject ro;
+	public Text punktetext;
+	void Start () {
 		
 	}
 	
 
 	     void Update(){
+			 if(!(zug == 20)){
        if (Input.GetMouseButtonDown(0)){ // if left button pressed...
          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
          RaycastHit hit;
          if (Physics.Raycast(ray, out hit)){
-           Debug.Log(hit.transform);
+           //Debug.Log(hit.transform.gameObject);
+		   
+		   switch (hit.transform.gameObject.name){
+		case("restmull"):
+		if (ro.mullsorte == 3){
+			ro.Neuermull();
+			punkte +=10;
+			}else{punkte -=5;
+			zug++;}	
+		break;
+		case("gelbmull"):
+		if (ro.mullsorte == 1){
+			ro.Neuermull();
+			punkte +=10;
+			}else{punkte -=5;
+			zug++;}	
+		break;
+		case("papiermull"):
+		if (ro.mullsorte == 2){
+			ro.Neuermull();
+			punkte +=10;
+			}else{punkte -=5;
+			zug++;}	
+		break;
+		 }
          }
+	
+		
+		
+		 
        }
-
-
-
+	   punktetext.text = "Punkte: " + punkte + ", Fehler: " + zug;
+	 
      }
 	 
 	 
 	 
-     
+		 }    
 }
+
--- Workbench/Assets/SCRIPTE/randomobject.cs
+++ Workbench/Assets/SCRIPTE/randomobject.cs
@@ -2,24 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class randomobject : MonoBehaviour {
+public class Randomobject : MonoBehaviour {
 public List <GameObject> mulle;
+public int mullsorte = 0;
 	
 	
-	// Use this for initialization
 	void Start () {
+		Neuermull();
+	}
+	
+
+	void Update () {
+		if (mulle[0].activeSelf){
+		mullsorte = 1;
+		
+		
+		}
+		if (mulle[1].activeSelf){
+		mullsorte = 2;
+		
+		
+		}
+		if (mulle[2].activeSelf){
+		mullsorte = 3;
+		
+		
+		}
+		if (mulle[3].activeSelf){
+		mullsorte = 2;
+		
+		
+		}
+		//Debug.Log(mullsorte);
+	}
+	public void Neuermull(){
 		for(int i = 0; i < mulle.Count;i++){
 		mulle[i].SetActive(false);	
-			
 		}
 		    
-			mulle[Random.Range(0,mulle.Count)].SetActive(true);
-		
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	mulle[Random.Range(0,mulle.Count)].SetActive(true);
+		}
 }
